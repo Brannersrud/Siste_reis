78f50548506b701d68c131d86c1c9832b574fb01
Simen <Simen.ks@hotmail.com>
2019-06-08 14:48:50 +0200

    :)

---
+++ client/src/components/login.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+
+class Loginform extends Component {
+    constructor(props) {
+        super(props); 
+
+        this.state={
+            'username': '',
+            'password': ''
+        }
+    }
+
+    render(){
+        return(
+            <div>
+            
+            </div>
+        )
+    }
+}
